ad6e806c2017a813ef5f7379d6a6098cc180070c
jiahao2112 <jiahaolee2112@gmail.com>
2024-11-09 01:21:53 +0800

    Update workoutRoutes.js

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 // GET route to get a user's workout schedule
 router.get('/schedule/:user_id', workoutController.getWorkoutSchedule);
 
+// DELETE route to delete workout_exercises by id
 router.delete('/workout_exercises/:id', workoutController.deleteWorkoutExercise);
 
 // POST route to add a workout
@@ -20,12 +21,5 @@ router.put('/fatigue_rating/:workout_id', workoutController.updateFatigueRating)
 
 // GET route to retrieve exercise by workout_id 
 router.get('/exercises/:workout_id', workoutController.getExercisesByWorkoutId);
-
-// DELETE route to delete workout_exercises by id
-// router.delete('/workout_exercises/:user_id/:workout_id/:exercise_id', workoutController.deleteWorkoutExercise);
-
-
-// router.get('/workout_exercises/:id', workoutController.get);
-
 
 module.exports = router;
